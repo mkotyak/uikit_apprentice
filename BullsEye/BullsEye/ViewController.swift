1db5fda37b0c5fcdded0fb0554d0f09b38bd3a75
mkotyak <kotyak.maria@gmail.com>
2023-07-10 18:22:04 +0200

    Chapter 3: Slider & Labels

--- BullsEye/BullsEye/ViewController.swift
+++ BullsEye/BullsEye/ViewController.swift
@@ -1,28 +1,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private var currentSliderValue: Int = 50
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
     @IBAction func showAlert() {
+        let message = "The value of the slider is: \(currentSliderValue)"
+
         let alert = UIAlertController(
             title: "Hello, World",
-            message: "This is my first app!",
+            message: message,
             preferredStyle: .alert
         )
 
         let action = UIAlertAction(
-            title: "Awesom",
+            title: "OK",
             style: .default
         )
 
         alert.addAction(action)
+
         present(
             alert,
             animated: true,
             completion: nil
         )
+    }
+
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        currentSliderValue = lroundf(slider.value)
     }
 }
