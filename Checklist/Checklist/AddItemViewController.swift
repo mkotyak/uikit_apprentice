18e40bb1f70ab6e498ce0426f240ed9b0e32e474
mkotyak <kotyak.maria@gmail.com>
2023-07-17 18:52:32 +0200

    Added static cell with text field to the AddItemViewController

--- Checklist/Checklist/AddItemViewController.swift
+++ Checklist/Checklist/AddItemViewController.swift
@@ -6,19 +6,6 @@ class AddItemViewController: UITableViewController {
 
         navigationItem.largeTitleDisplayMode = .never
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 0
-    }
-
-    override func tableView(
-        _ tableView: UITableView,
-        numberOfRowsInSection section: Int
-    ) -> Int {
-        return 0
-    }
 }
 
 // MARK: - Actions
