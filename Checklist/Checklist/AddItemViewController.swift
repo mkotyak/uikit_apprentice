1156ea206d91863d039c1d65d3475fccd4945a4a
mkotyak <kotyak.maria@gmail.com>
2023-07-19 11:51:15 +0200

    Added textField autlet to the AddItemViewController and activated keyboard on appear

--- Checklist/Checklist/AddItemViewController.swift
+++ Checklist/Checklist/AddItemViewController.swift
@@ -1,21 +1,39 @@
 import UIKit
 
 class AddItemViewController: UITableViewController {
+    @IBOutlet var textField: UITextField!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         navigationItem.largeTitleDisplayMode = .never
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        textField.becomeFirstResponder()
     }
 }
 
 // MARK: - Actions
 
 extension AddItemViewController {
-    @IBAction func cancel() {
+    @IBAction private func cancel() {
         navigationController?.popViewController(animated: true)
     }
 
-    @IBAction func done() {
+    @IBAction private func done() {
         navigationController?.popViewController(animated: true)
+    }
+}
+
+// MARK: - Table view Delegate
+
+extension AddItemViewController {
+    override func tableView(
+        _ tableView: UITableView,
+        willSelectRowAt indexPath: IndexPath
+    ) -> IndexPath? {
+        return nil
     }
 }
