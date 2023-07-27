3ea0d1fa21881fa7bf390a9d4e501b3fdf57b1fd
mkotyak <kotyak.maria@gmail.com>
2023-07-27 12:54:46 +0200

    Removed test code for scheduling local notifications

--- Checklist/Checklist/AppDelegate.swift
+++ Checklist/Checklist/AppDelegate.swift
@@ -7,48 +7,42 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
-        // Notification authorization
         let center = UNUserNotificationCenter.current()
-        center.requestAuthorization(options: [.alert, .sound]) { granted, _ in
-            if granted {
-                debugPrint("We have permission")
-            } else {
-                debugPrint("Permissions denied")
-            }
-        }
-
-        let content = UNMutableNotificationContent()
-        content.title = "Hello!"
-        content.body = "I am a local notification"
-        content.sound = .default
-
-        let trigger = UNTimeIntervalNotificationTrigger(
-            timeInterval: 10,
-            repeats: false
-        )
-
-        let request = UNNotificationRequest(
-            identifier: "MyNotification",
-            content: content,
-            trigger: trigger
-        )
-
-        center.add(request)
+        center.delegate = self
 
         return true
     }
 
     // MARK: UISceneSession Lifecycle
 
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+    func application(
+        _ application: UIApplication,
+        configurationForConnecting connectingSceneSession: UISceneSession,
+        options: UIScene.ConnectionOptions
+    ) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+    func application(
+        _ application: UIApplication,
+        didDiscardSceneSessions sceneSessions: Set<UISceneSession>
+    ) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+}
+
+// MARK: - User Notification Delegates
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    func userNotificationCenter(
+        _ center: UNUserNotificationCenter,
+        willPresent notification: UNNotification,
+        withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void
+    ) {
+        debugPrint("Received local notification \(notification)")
     }
 }
