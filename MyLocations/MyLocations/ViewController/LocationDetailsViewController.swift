ec988599222904e0becb72f5797fc931eb0cc31c
mkotyak <kotyak.maria@gmail.com>
2023-09-19 15:45:44 +0200

    Initialized LocationDetailsViewController.swift

---
+++ MyLocations/MyLocations/ViewController/LocationDetailsViewController.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+class LocationDetailsViewController: UIViewController {
+    @IBOutlet var descriptionTextView: UITextView!
+    @IBOutlet var categoryLabel: UILabel!
+    @IBOutlet var latitudeLabel: UILabel!
+    @IBOutlet var longitudeLabel: UILabel!
+    @IBOutlet var addressLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+// MARK: - Actions
+
+extension LocationDetailsViewController {
+    @IBAction func done() {
+        navigationController?.popViewController(animated: true)
+    }
+
+    @IBAction func cancel() {
+        navigationController?.popViewController(animated: true)
+    }
+}
