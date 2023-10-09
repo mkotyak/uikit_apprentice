edda1c39c4c1a647bf23740c41041b482eb50487
mkotyak <kotyak.maria@gmail.com>
2023-10-09 14:22:54 +0200

    Created custom pin

--- MyLocations/MyLocations/ViewController/MapViewController.swift
+++ MyLocations/MyLocations/ViewController/MapViewController.swift
@@ -18,85 +18,85 @@ class MapViewController: UIViewController {
         }
     }
     
-    private func region(for annotations: [MKAnnotation]) -> MKCoordinateRegion {
-        let region: MKCoordinateRegion
-        
-        switch annotations.count {
-        case 0:
-            region = MKCoordinateRegion(
-                center: mapView.userLocation.coordinate,
-                latitudinalMeters: 1000,
-                longitudinalMeters: 1000
-            )
-            
-        case 1:
-            let annotation = annotations[annotations.count - 1]
-            region = MKCoordinateRegion(
-                center: annotation.coordinate,
-                latitudinalMeters: 1000,
-                longitudinalMeters: 1000
-            )
-            
-        default:
-            var topLeft = CLLocationCoordinate2D(
-                latitude: -90,
-                longitude: 180
-            )
-            var bottomRight = CLLocationCoordinate2D(
-                latitude: 90,
-                longitude: -180
-            )
-            
-            for annotation in annotations {
-                topLeft.latitude = max(
-                    topLeft.latitude,
-                    annotation.coordinate.latitude
-                )
-                
-                topLeft.longitude = min(
-                    topLeft.longitude,
-                    annotation.coordinate.longitude
-                )
-                
-                bottomRight.latitude = min(
-                    bottomRight.latitude,
-                    annotation.coordinate.latitude
-                )
-                
-                bottomRight.longitude = max(
-                    bottomRight.longitude,
-                    annotation.coordinate.longitude
-                )
-            }
-            
-            let center = CLLocationCoordinate2D(
-                latitude: topLeft.latitude - (topLeft.latitude - bottomRight.latitude) / 2,
-                longitude: topLeft.longitude - (topLeft.longitude - bottomRight.longitude) / 2)
-            
-            let extraSpace = 1.1
-            let span = MKCoordinateSpan(
-                latitudeDelta: abs(topLeft.latitude - bottomRight.latitude) * extraSpace,
-                longitudeDelta: abs(topLeft.longitude - bottomRight.longitude) * extraSpace
-            )
-            
-            region = MKCoordinateRegion(center: center, span: span)
+    func region(for annotations: [MKAnnotation]) -> MKCoordinateRegion {
+      let region: MKCoordinateRegion
+
+      switch annotations.count {
+      case 0:
+        region = MKCoordinateRegion(
+          center: mapView.userLocation.coordinate,
+          latitudinalMeters: 1000,
+          longitudinalMeters: 1000
+        )
+
+      case 1:
+        let annotation = annotations[annotations.count - 1]
+        region = MKCoordinateRegion(
+          center: annotation.coordinate,
+          latitudinalMeters: 1000,
+          longitudinalMeters: 1000
+        )
+
+      default:
+        var topLeft = CLLocationCoordinate2D(
+          latitude: -90,
+          longitude: 180
+        )
+        var bottomRight = CLLocationCoordinate2D(
+          latitude: 90,
+          longitude: -180
+        )
+
+        for annotation in annotations {
+          topLeft.latitude = max(
+            topLeft.latitude,
+            annotation.coordinate.latitude
+          )
+          topLeft.longitude = min(
+            topLeft.longitude,
+            annotation.coordinate.longitude
+          )
+          bottomRight.latitude = min(
+            bottomRight.latitude,
+            annotation.coordinate.latitude
+          )
+          bottomRight.longitude = max(
+            bottomRight.longitude,
+            annotation.coordinate.longitude
+          )
         }
+
+        let center = CLLocationCoordinate2D(
+          latitude: topLeft.latitude - (topLeft.latitude - bottomRight.latitude) / 2,
+          longitude: topLeft.longitude - (topLeft.longitude - bottomRight.longitude) / 2
+        )
+
+        let extraSpace = 1.1
+        let span = MKCoordinateSpan(
+          latitudeDelta: abs(topLeft.latitude - bottomRight.latitude) * extraSpace,
+          longitudeDelta: abs(topLeft.longitude - bottomRight.longitude) * extraSpace
+        )
+
+        region = MKCoordinateRegion(center: center, span: span)
+      }
         
-        return mapView.regionThatFits(region)
+      return mapView.regionThatFits(region)
     }
+    
+    @objc private func showLocationDetails(_ sender: UIButton) {}
     
     // MARK: - Actions
     
     @IBAction func showUser() {
         let region = MKCoordinateRegion(
-            center: mapView.userLocation.coordinate,
-            latitudinalMeters: 1000,
-            longitudinalMeters: 1000
+          center: mapView.userLocation.coordinate,
+          latitudinalMeters: 1000,
+          longitudinalMeters: 1000
         )
         
         mapView.setRegion(
-            mapView.regionThatFits(region),
-            animated: true
+          mapView.regionThatFits(region),
+          animated: true
         )
     }
     
@@ -111,7 +111,55 @@ class MapViewController: UIViewController {
 // MARK: - MKMapViewDelegate
 
 extension MapViewController: MKMapViewDelegate {
-    
+    func mapView(
+      _ mapView: MKMapView,
+      viewFor annotation: MKAnnotation
+    ) -> MKAnnotationView? {
+      guard annotation is Location else {
+        return nil
+      }
+
+      let identifier = "Location"
+      var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+        
+
+
+      if let annotationView {
+        annotationView.annotation = annotation
+
+        let button = annotationView.rightCalloutAccessoryView as! UIButton
+        if let index = locations.firstIndex(of: annotation as! Location) {
+          button.tag = index
+        }
+      } else {
+          let pinView = MKPinAnnotationView(
+            annotation: annotation,
+            reuseIdentifier: identifier
+          )
+          
+          pinView.isEnabled = true
+          pinView.canShowCallout = true
+          pinView.animatesDrop = false
+          pinView.pinTintColor = UIColor(
+            red: 0.32,
+            green: 0.82,
+            blue: 0.4,
+            alpha: 1
+          )
+
+          let rightButton = UIButton(type: .detailDisclosure)
+          rightButton.addTarget(
+            self,
+            action: #selector(showLocationDetails(_:)),
+            for: .touchUpInside
+          )
+          pinView.rightCalloutAccessoryView = rightButton
+
+          annotationView = pinView
+      }
+        
+      return annotationView
+    }
 }
 
 // MARK: - Helper methods
@@ -119,14 +167,13 @@ extension MapViewController: MKMapViewDelegate {
 extension MapViewController {
     func updateLocations() {
         mapView.removeAnnotations(locations)
-        
+
         let entity = Location.entity()
         
         let fetchRequest = NSFetchRequest<Location>()
         fetchRequest.entity = entity
-        
+
         locations = try! managedObjectContext.fetch(fetchRequest)
-        
         mapView.addAnnotations(locations)
     }
 }
