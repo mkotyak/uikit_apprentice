926734ee966f3d223613766ff7f1401a7264d0ae
mkotyak <kotyak.maria@gmail.com>
2023-10-24 18:27:19 +0300

    Added loading spinner to the Tag screen

--- MyLocations/MyLocations/ViewController/CurrentLocationViewController.swift
+++ MyLocations/MyLocations/ViewController/CurrentLocationViewController.swift
@@ -118,10 +118,25 @@ class CurrentLocationViewController: UIViewController {
     }
 
     private func configureGetButton() {
+        let spinnerTag = 1000
+
         if isUpdatingLocation {
             getButton.setTitle("Stop", for: .normal)
+
+            if view.viewWithTag(spinnerTag) == nil {
+                let spinner = UIActivityIndicatorView(style: .medium)
+                spinner.center = messageLabel.center
+                spinner.center.y += spinner.bounds.size.height / 2 + 25
+                spinner.startAnimating()
+                spinner.tag = spinnerTag
+                containerView.addSubview(spinner)
+            }
         } else {
             getButton.setTitle("Get My Location", for: .normal)
+
+            if let spinner = view.viewWithTag(spinnerTag) {
+                spinner.removeFromSuperview()
+            }
         }
     }
 
