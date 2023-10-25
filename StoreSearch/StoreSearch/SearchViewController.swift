ade7763555be09ff7ce7a8b4ff6bcd87d1b252e3
mkotyak <kotyak.maria@gmail.com>
2023-10-25 16:38:12 +0300

    Implemented search result retrieving with fake data

--- StoreSearch/StoreSearch/SearchViewController.swift
+++ StoreSearch/StoreSearch/SearchViewController.swift
@@ -4,6 +4,8 @@ class SearchViewController: UIViewController {
     @IBOutlet var searchBar: UISearchBar!
     @IBOutlet var tableView: UITableView!
 
+    private var searchResult: [String] = []
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -14,6 +16,46 @@ class SearchViewController: UIViewController {
 
 extension SearchViewController: UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        debugPrint("The search text is: '\(searchBar.text!)'")
+        searchResult = []
+
+        for index in 0 ... 2 {
+            searchResult.append(
+                String(format: "Fake Result %d for '%@'", index, searchBar.text!)
+            )
+        }
+
+        tableView.reloadData()
+    }
+}
+
+// MARK: Table View Delegate
+
+extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(
+        _ tableView: UITableView,
+        numberOfRowsInSection section: Int
+    ) -> Int {
+        searchResult.count
+    }
+
+    func tableView(
+        _ tableView: UITableView,
+        cellForRowAt indexPath: IndexPath
+    ) -> UITableViewCell {
+        var cell: UITableViewCell
+        let cellIdentifier = "SearchResultCell"
+
+        if let reusableCell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier) {
+            cell = reusableCell
+        } else {
+            cell = UITableViewCell(
+                style: .default,
+                reuseIdentifier: cellIdentifier
+            )
+        }
+
+        cell.textLabel!.text = searchResult[indexPath.row]
+
+        return cell
     }
 }
