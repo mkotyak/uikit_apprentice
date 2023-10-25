6220802410dadb4e231696c2afdf9fac0c9aef5b
mkotyak <kotyak.maria@gmail.com>
2023-10-25 12:37:26 +0300

    Created StoreSearch project

---
+++ StoreSearch/StoreSearch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoreSearch
+//
+//  Created by Maria Kotyak on 25/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
