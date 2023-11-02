858a40a7cb1196f4e52f0f9ee2eb9f00d4d57d84
mkotyak <kotyak.maria@gmail.com>
2023-11-02 15:51:02 +0300

    Added all required properties to the SearchResult

--- StoreSearch/StoreSearch/ SearchResult.swift
+++ StoreSearch/StoreSearch/ SearchResult.swift
@@ -6,15 +6,30 @@ class ResultArray: Codable {
 }
 
 class SearchResult: Codable, CustomStringConvertible {
+    private enum CodingKeys: String, CodingKey {
+        case imageSmall = "artworkUrl60"
+        case imageLarge = "artworkUrl100"
+        case storeURL = "trackViewUrl"
+        case genre = "primaryGenreName"
+        case kind, artistName, trackName
+        case trackPrice, currency
+    }
+
+    var kind: String? = ""
     var artistName: String? = ""
     var trackName: String? = ""
-    var kind: String? = ""
+    var trackPrice: Double? = 0.0
+    var storeURL: String? = ""
+    var currency: String = ""
+    var imageSmall: String = ""
+    var imageLarge: String = ""
+    var genre: String = ""
 
     var name: String {
         trackName ?? ""
     }
 
     var description: String {
-        "\nResult - Kind: \(kind ?? "None"), Name: \(name), Artist Name: \(artistName ?? "None")"
+        "\nResult - Kind: \(kind ?? "None"), Name: \(name), Artist Name: \(artistName ?? "None"), Genre: \(genre)"
     }
 }
