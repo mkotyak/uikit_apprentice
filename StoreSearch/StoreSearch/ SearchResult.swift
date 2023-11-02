5e907ba46e088721c6f642354741a1656535a13a
mkotyak <kotyak.maria@gmail.com>
2023-11-02 16:15:59 +0300

    Finished property list for SearchResult

--- StoreSearch/StoreSearch/ SearchResult.swift
+++ StoreSearch/StoreSearch/ SearchResult.swift
@@ -9,27 +9,52 @@ class SearchResult: Codable, CustomStringConvertible {
     private enum CodingKeys: String, CodingKey {
         case imageSmall = "artworkUrl60"
         case imageLarge = "artworkUrl100"
-        case storeURL = "trackViewUrl"
-        case genre = "primaryGenreName"
-        case kind, artistName, trackName
-        case trackPrice, currency
+        case itemGenre = "primaryGenreName"
+        case bookGenre = "genres"
+        case itemPrice = "price"
+        case kind, artistName, currency
+        case trackName, trackPrice, trackViewUrl
+        case collectionName, collectionViewUrl, collectionPrice
     }
 
     var kind: String? = ""
     var artistName: String? = ""
     var trackName: String? = ""
     var trackPrice: Double? = 0.0
-    var storeURL: String? = ""
     var currency: String = ""
     var imageSmall: String = ""
     var imageLarge: String = ""
-    var genre: String = ""
+    var trackViewUrl: String?
+    var collectionName: String?
+    var collectionViewUrl: String?
+    var collectionPrice: Double?
+    var itemPrice: Double?
+    var itemGenre: String?
+    var bookGenre: [String]?
 
     var name: String {
-        trackName ?? " "
+        trackName ?? collectionName ?? " "
+    }
+
+    var storeURL: String {
+        trackViewUrl ?? collectionViewUrl ?? ""
+    }
+
+    var price: Double {
+        trackPrice ?? collectionPrice ?? itemPrice ?? 0.0
+    }
+
+    var genre: String {
+        if let genre = itemGenre {
+            return genre
+        } else if let genres = bookGenre {
+            return genres.joined(separator: ", ")
+        }
+
+        return ""
     }
 
     var description: String {
-        "\nResult - Kind: \(kind ?? "None"), Name: \(name), Artist Name: \(artistName ?? "None"), Genre: \(genre)"
+        "\nResult - Kind: \(kind ?? "None"), Name: \(name), Artist Name: \(artistName ?? "None")"
     }
 }
