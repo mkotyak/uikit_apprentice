0da03e0c180d8721624bc2ef3915476aef566530
mkotyak <kotyak.maria@gmail.com>
2023-11-09 17:16:06 +0300

    Extended UIImageView with DownloadImage functionality

---
+++ StoreSearch/StoreSearch/Extensions/UIImageView+DownloadImage.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+extension UIImageView {
+    func loadImage(url: URL) -> URLSessionDownloadTask {
+        let session = URLSession.shared
+
+        let downloadTask = session.downloadTask(with: url) { [weak self] url, _, error in
+            if error == nil,
+               let url = url,
+               let data = try? Data(contentsOf: url),
+               let image = UIImage(data: data)
+            {
+                DispatchQueue.main.async { [weak self] in
+                    if let self {
+                        self.image = image
+                    }
+                }
+            }
+        }
+
+        downloadTask.resume()
+
+        return downloadTask
+    }
+}
