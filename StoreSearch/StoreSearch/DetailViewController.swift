e70c07768a793beb1d36b265b2989f7f93c96dc1
mkotyak <kotyak.maria@gmail.com>
2023-11-10 16:40:37 +0300

    Dislayed details view in the modal window

--- StoreSearch/StoreSearch/DetailViewController.swift
+++ StoreSearch/StoreSearch/DetailViewController.swift
@@ -1,22 +1,15 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-    
+}
 
-    /*
-    // MARK: - Navigation
+// MARK: - Actions
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension DetailViewController {
+    @IBAction func close() {
+        dismiss(animated: true)
     }
-    */
-
 }
--- StoreSearch/StoreSearch/SearchViewController.swift
+++ StoreSearch/StoreSearch/SearchViewController.swift
@@ -193,6 +193,7 @@ extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
         didSelectRowAt indexPath: IndexPath
     ) {
         tableView.deselectRow(at: indexPath, animated: true)
+        performSegue(withIdentifier: "ShowDetail", sender: indexPath)
     }
 
     func tableView(
