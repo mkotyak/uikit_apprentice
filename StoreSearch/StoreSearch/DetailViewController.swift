c65d30b30f1451d42d7a9e8c5df9db0d80225c0a
mkotyak <kotyak.maria@gmail.com>
2023-11-10 17:17:29 +0300

    Made connection for all elements on the detail view

--- StoreSearch/StoreSearch/DetailViewController.swift
+++ StoreSearch/StoreSearch/DetailViewController.swift
@@ -1,6 +1,14 @@
 import UIKit
 
 class DetailViewController: UIViewController {
+    @IBOutlet var popupView: UIView!
+    @IBOutlet var artworkImageView: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var artistNameLabel: UILabel!
+    @IBOutlet var kindLabel: UILabel!
+    @IBOutlet var genreLabel: UILabel!
+    @IBOutlet var priceButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
