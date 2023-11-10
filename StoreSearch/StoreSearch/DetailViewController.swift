75049b8629ada266359273a6c23fe25b5047575f
mkotyak <kotyak.maria@gmail.com>
2023-11-10 18:06:36 +0300

    Added ability to close modat by tap outside the detail view

--- StoreSearch/StoreSearch/DetailViewController.swift
+++ StoreSearch/StoreSearch/DetailViewController.swift
@@ -12,6 +12,16 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         popupView.layer.cornerRadius = 10
+
+        let gestureRecognizer = UITapGestureRecognizer(
+            target: self,
+            action: #selector(close)
+        )
+
+        gestureRecognizer.cancelsTouchesInView = false
+        gestureRecognizer.delegate = self
+
+        view.addGestureRecognizer(gestureRecognizer)
     }
 }
 
@@ -20,5 +30,16 @@ class DetailViewController: UIViewController {
 extension DetailViewController {
     @IBAction func close() {
         dismiss(animated: true)
+    }
+}
+
+// MARK: - UIGestureRecognizerDelegate
+
+extension DetailViewController: UIGestureRecognizerDelegate {
+    func gestureRecognizer(
+        _ gestureRecognizer: UIGestureRecognizer,
+        shouldReceive touch: UITouch
+    ) -> Bool {
+        touch.view === view
     }
 }
