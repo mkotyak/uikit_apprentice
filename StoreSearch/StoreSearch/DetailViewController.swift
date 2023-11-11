0590c0f3375af65df055af54c9c1ea22b29ae3da
mkotyak <kotyak.maria@gmail.com>
2023-11-11 15:41:06 +0300

    Added gradient

--- StoreSearch/StoreSearch/DetailViewController.swift
+++ StoreSearch/StoreSearch/DetailViewController.swift
@@ -29,8 +29,14 @@ class DetailViewController: UIViewController {
         if searchResult != nil {
             updateUI()
         }
+
+        // Gradient view
+        view.backgroundColor = UIColor.clear
+        let dimmingView = GradientView(frame: CGRect.zero)
+        dimmingView.frame = view.bounds
+        view.insertSubview(dimmingView, at: 0)
     }
-    
+
     deinit {
         downloadTask?.cancel()
     }
